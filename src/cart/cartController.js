6b6b886a08b82a9d353cde30c1cd9721b7966071
Santiago Torres <santt2498@gmail.com>
2023-07-31 19:10:03 -0300

    Commit terminando cartController.js, todo funciona correcto

--- src/cart/cartController.js
+++ src/cart/cartController.js
@@ -7,107 +7,77 @@ const cartFilePath = process.cwd() + "/Files/cart.json";
 const parseCart = async () => {
     try {
         const data = await fs.promises.readFile(cartFilePath, "utf-8");  
-        return console.log(data)
+        const cartList = JSON.parse(data);
 
-        // return productList
+        return cartList
     } catch (error) {
         console.log(error);
     }
 };
 
-parseCart() //AQUI MUESTRA QUE LOS PRODUCTOS LLEGAN DEL JSON PERO NO FUNCIONA EN POSTMAN
-
-router.get("/", async (req, res) =>{
-    const cartJSON = await parseCart();
-    const { limit } = req.query;
-
-    try {
-        const limitFilter = cartJSON.slice(0, limit || 5)
-        limitFilter 
-            ? res.json( { message: limitFilter } ) 
-            : res.json({ message: cartJSON })
-    } catch (error) {
-        console.log(error);
+const updateCartFile = async (arr) =>{
+    try{
+        await fs.promises.readFile(cartFilePath, "utf-8")
+        await fs.promises.writeFile(cartFilePath, JSON.stringify(arr))
+    }catch(error){
+        console.log(error)
     }
-});
+}
 
 router.get("/:cid", async (req, res) => {
     const cartJSON = await parseCart();
     const { cid } = req.params
 
-    const idFilter = cartJSON.filter(prod => prod.id === Number(cid))
+    const cart = cartJSON.find(cart => cart.id === Number(cid))
 
     try {
-        idFilter 
-            ? res.json( { message: idFilter } ) 
-            : res.json( { massage: cartJSON } )
-    } catch (error) {
-        console.log(error)
-    }
-})
-
-router.post("/:cid/product/:pid", async (req, res) => {
-    const { title, description, price, thumbnail, code, stock } = req.body
-    const cartJSON = await parseCart();
-    
-  
-    const prodInfo = {
-        id: "test", 
-        title,
-        description,
-        price,
-        thumbnail,
-        code,
-        stock,
-        status: true
-    };
-    const productByCode = cartJSON.find(prod => prod.code === code)
-
-    if(productByCode){
-        res.json({ message: "El producto ya existe" });
-    }else{
-        productsJSON.push(prodInfo);
-        const cartStringify = JSON.stringify(cartJSON);
-
-        await fs.promises.writeFile(productsFilePath, cartStringify);
-        res.json({ message: "Producto creado" });
-    }
-})
-
-router.put("/:pid", async (req, res) =>{
-    const { title, prop } = req.params;
-    const { value } = req.body;
-
-    console.log("Title:", title);
-    console.log("Prop:", prop);
-    console.log("Value:", value); //ESTO APARECE UNDEFINED NO SE PORQUE
-
-    const cartJSON = await parseCart();
-    const filteredProduct = cartJSON.find(p => p.title === title)
-
-    try {
-        if(!filteredProduct){
-            res.json({ message:"El producto no existe" })
+        if(!cart){
+            res.json( { message: "Cart not found" } )
         }else{
-            filteredProduct[prop]= value
-            res.json({ message: filteredProduct })
+            const products = cart.products;
+            res.json( { massage: products } )
         }
     } catch (error) {
         console.log(error)
     }
 })
 
-router.delete("/:pid", async (req, res) =>{
-    const { title } = req.params
+router.post("/:cid/product/:pid", async (req, res) => {
+    const { quantity } = req.body;
+    const { cid, pid } = req.params;
     const cartJSON = await parseCart();
-    const prodIndex = cartJSON.findIndex(prod => prod.title === title)
-            
-    try {
-        cartJSON.splice(prodIndex, 1)
-        res.json({ message: cartJSON })              
-    } catch (error) {
-        console.log(error)
-    }      
+
+    const numberPid = Number(pid)
+    const numberCid = Number(cid)
+
+    const prodInfo = {
+        id: numberPid,
+        quantity
+    }
+
+    const cartIndex = cartJSON.findIndex(cart => cart.id === numberCid);
+    const cart = cartJSON[cartIndex];
+
+    try{
+        if (!cart) {
+            res.json({ message: "Cart not found" });
+        } else {
+            const prodIndex = cart.products.findIndex(product => product.id === numberPid);
+
+            if (prodIndex === -1) {
+                cart.products.push(prodInfo);
+            } else {
+                cart.products[prodIndex].quantity += quantity;
+            }
+    
+            cartJSON[cartIndex] = cart;
+            updateCartFile(cartJSON);
+    
+            res.json({ message: cartJSON });
+        }
+    }catch (error) {
+        console.log(error);
+    }
 })
     
 
